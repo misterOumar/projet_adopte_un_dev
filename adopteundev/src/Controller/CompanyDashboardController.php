5e2527887a175777907d1a14dc7800127186a5a1
Hyckael <evrardhyckaelk@gmail.com>
2025-01-02 00:43:54 +0100

    feature/company actualisation du tableau de bord de la company et des details du poste

--- adopteundev/src/Controller/CompanyDashboardController.php
+++ adopteundev/src/Controller/CompanyDashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CompanyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,14 +10,15 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class CompanyDashboardController extends AbstractController
 {
-   
-
+   public function __construct(private CompanyRepository $companyRepository){}
     #[IsGranted('ROLE_COMPANY')]
     #[Route('/company/dashboard', name: 'app_company_dashboard')]
     public function index(): Response
     {
+        $user = $this->getUser();
+        $company = $this->companyRepository->findOneBy(['user' => $user]);
         return $this->render('company/dashboard.html.twig', [
-            'controller_name' => 'CompanyDashboardController',
+            'company' => $company,
         ]);
     }
 }
--- adopteundev/src/Controller/PosteController.php
+++ adopteundev/src/Controller/PosteController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use App\Entity\Poste;
 use App\Form\PosteFormType;
 use App\Repository\CompanyRepository;
+use App\Repository\PosteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class PosteController extends AbstractController
 {
+    public function __construct(private CompanyRepository $companyRepository, private PosteRepository $posteRepository){}
+
+    #[IsGranted('ROLE_COMPANY')]
     #[Route('company/poste/new', name: 'app_add_poste')]
     public function addPoste(Request $request, EntityManagerInterface $entityManager, CompanyRepository $companyRepository): Response
     {
@@ -38,5 +43,27 @@ class PosteController extends AbstractController
         return $this->render('company/add_poste.html.twig', [
             'form_add' => $form->createView(),
         ]);
+    }
+
+    #[IsGranted('ROLE_COMPANY')]
+    #[Route('company/poste/list', name: 'app_company_poste_list')]
+    public function posteList(){
+        // Récupérer la `Company` actuellement connectée ou liée
+        $user = $this->getUser();
+        $company = $this->companyRepository->findOneBy(['user' => $user]);
+        //recuperer tous postes de la company connectée
+        $postes = $company->getPostes();
+        return $this->render('company/poste_list.html.twig', ['postes' => $postes, 'company' => $company]);
+    }
+
+    #[IsGranted('ROLE_COMPANY')]
+    #[Route('company/poste/details/{uuid}', name: 'app_company_poste_details')]
+    public function posteDetail(string $uuid){
+        $poste = $this->posteRepository->findOneBy(['uuid' => $uuid]);
+        
+        if(!$poste){
+            throw $this->createNotFoundException('Poste introuvable');
+        }
+        return $this->render('company/poste_details.html.twig', ['poste' => $poste]);
     }
 }
