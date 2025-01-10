97d7eea248b49be1c23d3971ec68a9d93f855c9f
misterOumar <coulibalyoumartc@gmail.com>
2025-01-10 15:52:57 +0100

    feature/dev: filtre dev et post

---
+++ adopteundev/src/Controller/FavoriteDevController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CompanyRepository;
+use App\Repository\DeveloperRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class FavoriteDevController extends AbstractController
+{
+    #[Route('/favorite/add-dev/{uuid}', name: 'app_favorite_dev_add', methods:['POST'])]
+    public function addFavoriteDev(string $uuid, DeveloperRepository $developerRepository, CompanyRepository $companyRepository, EntityManagerInterface $entityManager): Response
+    {
+       $developer = $developerRepository->findOneBy(['uuid' => $uuid]);
+
+        if (!$developer) {
+            return $this->addFlash('warning', 'Développeur introuvable, veuillez réessayer plus tard !');
+            return $this->redirectToRoute('app_home');
+        }
+
+        $company = $companyRepository->findOneBy(['user' => $this->getUser()]);
+
+        if($company->getDeveloperSaved()->contains($developer)) {
+            $company->removeDeveloperSaved($developer);
+            $entityManager->flush();
+             $this->addFlash('success', 'Développeur retiré à vos favoris');
+            
+        }
+        return $this->redirectToRoute('app_dev_list');
+    }
+
+
+    #[Route('/favorite/remove-dev/{uuid}', name: 'app_favorite_dev_remove', methods:['POST'])]
+    public function removeFavoriteDev(string $uuid, DeveloperRepository $developerRepository, CompanyRepository $companyRepository, EntityManagerInterface $entityManager): Response
+    {
+       $developer = $developerRepository->findOneBy(['uuid' => $uuid]);
+
+        if (!$developer) {
+            return $this->addFlash('warning', 'Développeur introuvable, veuillez réessayer plus tard !');
+            return $this->redirectToRoute('app_home');
+        }
+
+        $company = $companyRepository->findOneBy(['user' => $this->getUser()]);
+
+        if(!$company->getDeveloperSaved()->contains($developer)) {
+            $company->addDeveloperSaved($developer);
+            $entityManager->flush();
+             $this->addFlash('success', 'Développeur ajouté à vos favoris');
+            
+        }
+        return $this->redirectToRoute('app_dev_list');
+    }
+}
---
+++ adopteundev/src/Entity/DeveloperView.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DeveloperViewRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DeveloperViewRepository::class)]
+class DeveloperView
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'views')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Developer $developer = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $viewedAt = null;
+
+    public function __construct()
+    {
+        $this->viewedAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDeveloper(): ?Developer
+    {
+        return $this->developer;
+    }
+
+    public function setDeveloper(?Developer $developer): static
+    {
+        $this->developer = $developer;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getViewedAt(): ?\DateTimeImmutable
+    {
+        return $this->viewedAt;
+    }
+
+    public function setViewedAt(\DateTimeImmutable $viewedAt): static
+    {
+        $this->viewedAt = $viewedAt;
+
+        return $this;
+    }
+}
